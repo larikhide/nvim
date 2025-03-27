1cff91ce8aa5f093439598853b145891878d9b04
larikhide <petrakliy@gmail.com>
2025-03-27 21:23:15 +0200

    feat: add zen-mode

---
+++ lua/larikhide/plugins/zen-mode.lua
@@ -0,0 +1,15 @@
+-- Lua
+return {
+  "folke/zen-mode.nvim",
+    config = function()
+        vim.keymap.set("n", "<leader>zz", function()
+            require("zen-mode").setup {
+                window = {
+                    width = 90,
+                    options = { }
+                },
+            }
+            require("zen-mode").toggle()
+        end)
+    end
+}
